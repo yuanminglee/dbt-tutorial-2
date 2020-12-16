3dd784dc1423e588190c3ff08ac7e207fe43a031
Yuan Ming Lee <yuanming@grain.com.sg>
2020-12-16 04:37:45 +0000

    Create models

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,10 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    amount / 100 as amount, -- amount is stored in cents, convert it to dollars
+    created as created_at
+
+from `dbt-tutorial.stripe.payment`
